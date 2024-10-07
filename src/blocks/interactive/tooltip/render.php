b73caabcf510120b9347c10bd4414f5e89217a8a
Newton <51231083+Newton-Adams@users.noreply.github.com>
2024-10-07 12:40:10 +0200

    Tooltip responsive styling

--- src/blocks/interactive/tooltip/render.php
+++ src/blocks/interactive/tooltip/render.php
@@ -31,31 +31,38 @@ $tooltips = $attributes['tooltips'];
             <div class="tooltip-image wp-block-image">
                 <figure class="aligncenter size-full is-resized">
                     <picture>
-                        <img loading="lazy" decoding="async" src="<?php echo esc_url($tooltipsImage); ?>" alt="">
+                        <img class="skip-lazy" decoding="async" src="<?php echo esc_url($tooltipsImage); ?>" alt="">
                     </picture>
                 </figure>
+                
+                <!-- Loop through the tooltips and render them -->
+                <?php $tooltip_index = 1; ?>
+                <?php foreach ($tooltips as $tooltip) : ?>
+                    <?php 
+                    $topPosition = $tooltip['topPosition'];
+                    $leftPosition = $tooltip['leftPosition']; 
+                    ?>
+                    <div 
+                        class="wp-block-group pulsing-tooltip <?php echo $style === 'sidebar' && $tooltip_index === 1 ? 'active' : ''; ?>" 
+                        style="<?php echo $topPosition ? 'top: ' . $topPosition . ';' : ''; ?><?php echo $leftPosition ? 'left: ' . $leftPosition . ';' : ''; ?>" 
+                        data-wp-on--mouseenter="actions.toggleEnter" 
+                        data-wp-on--mouseleave="actions.toggleLeave" 
+                        data-tooltip-index="<?php echo $tooltip_index; ?>" >
+                        <div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
+                            <div class="wp-block-safe-svg-svg-icon safe-svg-cover" style="text-align: left;">
+                                <div class="tooltip-icon"></div>
+                            </div>
+                            <?php if ($style === 'default' ) : ?>
+                                <p class="tooltip"><?php echo esc_html($tooltip['text']); ?></p>
+                            <?php endif; ?>  
+                        </div>
+                    </div>
+                    <?php $tooltip_index++; ?>
+                <?php endforeach; ?>
+                
             </div>
         <?php endif; ?>
 
-        <!-- Loop through the tooltips and render them -->
-        <?php $tooltip_index = 1; ?>
-        <?php foreach ($tooltips as $tooltip) : ?>
-            <?php 
-            $topPosition = $tooltip['topPosition'];
-            $leftPosition = $tooltip['leftPosition']; 
-            ?>
-            <div class="wp-block-group pulsing-tooltip" style="<?php echo $topPosition ? 'top: ' . $topPosition . ';' : ''; ?><?php echo $leftPosition ? 'left: ' . $leftPosition . ';' : ''; ?>" data-wp-on--mouseenter="actions.toggleEnter" data-wp-on--mouseleave="actions.toggleLeave" data-tooltip-index="<?php echo $tooltip_index; ?>">
-                <div class="wp-block-group__inner-container is-layout-constrained wp-block-group-is-layout-constrained">
-                    <div class="wp-block-safe-svg-svg-icon safe-svg-cover" style="text-align: left;">
-                        <div class="tooltip-icon"></div>
-                    </div>
-                    <?php if ($style === 'default' ) : ?>
-                        <p class="tooltip"><?php echo esc_html($tooltip['text']); ?></p>
-                    <?php endif; ?>  
-                </div>
-            </div>
-            <?php $tooltip_index++; ?>
-        <?php endforeach; ?>
 
         <?php if ($tooltips && $style === 'sidebar' ) : ?>
             <div class="sidebar-tooltip">
@@ -66,7 +73,7 @@ $tooltips = $attributes['tooltips'];
                     $leftPosition = $tooltip['leftPosition']; 
                     ?>
                     <div class="tooltip-sidebar__item">
-                        <p class="tooltip <?php if ($sidebar_tooltip_index === 1) { echo 'active'; } ?>" data-sidebar-tooltip-index="<?php echo $sidebar_tooltip_index; ?>"><?php echo esc_html($tooltip['text']); ?></p>
+                        <p class="tooltip <?php if ($sidebar_tooltip_index === 1) { echo 'active'; } ?>" data-wp-on--click="actions.sidebarTooltip" data-sidebar-tooltip-index="<?php echo $sidebar_tooltip_index; ?>"><?php echo esc_html($tooltip['text']); ?></p>
                     </div>
                     <?php $sidebar_tooltip_index++; ?>
                     
--- src/blocks/interactive/tooltip/view.js
+++ src/blocks/interactive/tooltip/view.js
@@ -44,6 +44,42 @@ store('image-tooltips', {
                     tooltip.classList.remove('active');
                 }
             }
+        }, 
+        sidebarTooltip: () => { 
+            const context = getContext();
+            const tooltipEle = getElement(context);
+            console.log('tooltipEle', tooltipEle);
+
+            if (tooltipEle && tooltipEle.ref) {
+                const tooltipRef = tooltipEle.ref;
+                const pulsingTooltipWrapper = tooltipRef.closest('.pulsing-tooltip-wrapper');
+
+                if (pulsingTooltipWrapper && pulsingTooltipWrapper.classList.contains('has-sidebar')) {
+                    // get tooltipRef attr data-sidebar-tooltip-index
+                    const sidebarTooltipIndex = tooltipRef.getAttribute('data-sidebar-tooltip-index');
+                    const tooltipIndex = pulsingTooltipWrapper.querySelector(`[data-tooltip-index="${sidebarTooltipIndex}"]`);
+                    if (tooltipIndex) {
+                        // remove active class from all sidebar tooltips and pulsing-tooltip
+                        const allTooltipsIcons = pulsingTooltipWrapper.querySelectorAll('.pulsing-tooltip');
+                        allTooltipsIcons.forEach(tooltip => { 
+                            // remove active class from all pulsing-tooltip
+                            tooltip.classList.remove('active'); 
+                        });
+
+                        // add active class to current tooltip
+                        tooltipIndex.classList.add('active');
+
+                        // remove active class from all sidebar .tooltip
+                        const allSidebarTooltips = pulsingTooltipWrapper.querySelectorAll('.sidebar-tooltip .tooltip');
+                        allSidebarTooltips.forEach(sidebarTooltip => {
+                            sidebarTooltip.classList.remove('active');
+                        });
+
+                        // add active class to current sidebar tooltip
+                        tooltipRef.classList.add('active');
+                    }
+                }
+            }
         }
     },
 });
